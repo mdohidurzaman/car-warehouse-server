f6c1c471a08b380d06ef16cca5effafaef943440
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-05-01 00:37:48 +0600

    Added mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +9,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@amazon.38elh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("wareHouse").collection("car-service");
+    app.get("/car-service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Genius car service is running!");
+  res.send("Mongodb is running!!");
 });
 
 app.listen(port, () => {
