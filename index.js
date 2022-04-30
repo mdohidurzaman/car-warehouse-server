8543f8c04806842b308f3d20e6974104170a445b
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-04-30 20:35:12 +0600

    First commit to add some installation

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Genius car service is running!");
+});
+
+app.listen(port, () => {
+  console.log("listening to port:", port);
+});
