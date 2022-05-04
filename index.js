5e27d7fd78bca2fba2eb86571dc56cba8cef5b50
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-05-04 15:04:29 +0600

    Added send data to server function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { application } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,12 +22,25 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("wareHouse").collection("car-service");
+    //Get inventories
     app.get("/car-service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
+    //Send inventories
+    app.post("/car-service", async (req, res) => {
+      const newInventory = req.body;
+      console.log("Adding new inventory", newInventory);
+      const result = await serviceCollection.insertOne(newInventory);
+      res.send(result);
+    });
+
+    //Update inventories
+    app.put("", (req, res) => {});
+    //Delete inventories
+    app.delete("", (req, res) => {});
   } finally {
   }
 }
