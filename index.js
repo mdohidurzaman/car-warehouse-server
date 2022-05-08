491871e03011e657294d65438861fff088980007
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-05-08 09:36:25 +0600

    Completed api with jwt implement

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeaders = req.headers.authorization;
+  if (!authHeaders) {
+    return res.status(401).send({ message: "Unauthorized access." });
+  }
+  const token = authHeaders.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access!!" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 
 //database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@amazon.38elh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -22,11 +39,7 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("wareHouse").collection("carServices");
-    //User login
-    app.post("/login", async (req, res) => {
-      const email = req.body;
-      console.log(email);
-    });
+
     //Get inventories
     app.get("/carServices", async (req, res) => {
       const query = {};
@@ -42,12 +55,28 @@ async function run() {
       const result = await serviceCollection.findOne(query);
       res.send(result);
     });
-    //Send inventories
-    app.post("/carServices", async (req, res) => {
-      const newInventory = req.body;
-      console.log("Adding new inventory", newInventory);
-      const result = await serviceCollection.insertOne(newInventory);
-      res.send(result);
+
+    //User login
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // Post inventories
+    app.post("/carServices", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const newInventory = req.body;
+        console.log("Adding new inventory", newInventory);
+        const result = await serviceCollection.insertOne(newInventory);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Access!!" });
+      }
     });
 
     //Update delivery inventory
@@ -58,7 +87,7 @@ async function run() {
       const options = { upsert: true };
       const updatedItem = {
         $set: {
-          quantity: updatedInventory.restQuantity,
+          quantity: updatedInventory.quantity,
         },
       };
       const result = await serviceCollection.updateOne(
@@ -76,7 +105,7 @@ async function run() {
       const options = { upsert: true };
       const updatedItem = {
         $set: {
-          quantity: updatedInventory.presentInventory,
+          quantity: updatedInventory.quantity,
         },
       };
       const result = await serviceCollection.updateOne(
